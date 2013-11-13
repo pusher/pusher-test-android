82efa7e1ae79000eb44593d5f1f09700dab2cfe9
Michael Pye <mail@mdpye.co.uk>
2013-11-13 10:22:12 +0000

    More features:
     - Logs now working with scrolling
     - Clear logs button
     - Reconnect fixed (don't re-register subscription)
     - Bind and log incoming events
     - Network status wired up

--- PusherTestApp/src/main/java/com/pusher/testapp/MainActivity.java
+++ PusherTestApp/src/main/java/com/pusher/testapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.pusher.testapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,9 +34,11 @@ public class MainActivity extends ActionBarActivity
     private static final String TAG = "PUSHER";
     private static final String API_KEY = "157a2f34672776fc4a73";
     private static final String CHANNEL_NAME = "test-channel";
+    private static final String EVENT_NAME = "test-event";
 
     private Pusher pusher;
     private PusherOptions options = new PusherOptions();
+    private boolean subscriptionCreated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +52,33 @@ public class MainActivity extends ActionBarActivity
                     .commit();
         }
 
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        filter.addCategory("com.pusher.testapp.MainActivity");
+        this.registerReceiver(new NetworkInfoReceiver(), filter);
+
         pusher = createPusher();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ((TextView)findViewById(R.id.log_view)).setMovementMethod(new ScrollingMovementMethod());
+
+        updateNetStatus(getApplicationContext());
+    }
+
     private Pusher createPusher() {
+        subscriptionCreated = false;
         return new Pusher(API_KEY, options);
     }
 
-    private PusherOptions createOptionsFromUiState() {
+    private void updateOptionsFromUiState() {
         ToggleButton ssl = (ToggleButton) findViewById(R.id.toggle_ssl);
 
-        PusherOptions options = new PusherOptions()
+        options = new PusherOptions()
                 .setEncrypted(ssl.isChecked());
-
-        return options;
     }
 
     /*
@@ -64,16 +86,11 @@ public class MainActivity extends ActionBarActivity
      */
 
     @Override
-    public void onSubscriptionSucceeded(String s) {
-        log("Subscription success: " + s);
-    }
-
-    @Override
     public void onConnectionStateChange(ConnectionStateChange connectionStateChange) {
         Log.i(TAG, "Got connection state change from [" + connectionStateChange.getPreviousState()
                 + "] to [" + connectionStateChange.getCurrentState() + "]");
 
-        log("State is now: " + connectionStateChange.getCurrentState().name());
+        log("State change: " + connectionStateChange.getCurrentState().name());
 
         updateConnectionIndicator(R.id.pusher_status, connectionStateChange.getCurrentState() == ConnectionState.CONNECTED);
 
@@ -81,13 +98,18 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public void onError(String s, String s2, Exception e) {
-        log("Error: " + s);
+    public void onSubscriptionSucceeded(String s) {
+        log("Subscription succeeded: " + s);
     }
 
     @Override
-    public void onEvent(String s, String s2, String s3) {
-        log("New event: " + s);
+    public void onEvent(String channel, String event, String data) {
+        log("New event: " + data);
+    }
+
+    @Override
+    public void onError(String message, String code, Exception e) {
+        log("Error: [" + message + "] [" + code + "] [" + e + "]");
     }
 
 
@@ -100,6 +122,10 @@ public class MainActivity extends ActionBarActivity
 
         if (pusher.getConnection().getState() == ConnectionState.DISCONNECTED) {
             pusher.connect(this);
+            if (!subscriptionCreated) {
+                pusher.subscribe(CHANNEL_NAME, this, EVENT_NAME);
+                subscriptionCreated = true;
+            }
         }
         else if (pusher.getConnection().getState() == ConnectionState.CONNECTED) {
             pusher.disconnect();
@@ -107,14 +133,21 @@ public class MainActivity extends ActionBarActivity
         // Ignore presses in other states, button should be disabled
     }
 
-    public void onClick_Ssl(final ToggleButton sslToggle) {
-        options = createOptionsFromUiState();
+    public void onClick_Ssl(final View sslToggle) {
+        updateOptionsFromUiState();
     }
 
-    /**
-     * @param target resource ID, must be instance of TextArea
-     * @param state true for green, false for red
-     */
+    public void onClick_ClearLog(final View btnClearLog) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView logs = (TextView)findViewById(R.id.log_view);
+                logs.setText("");
+                logs.invalidate();
+            }
+        });
+    }
+
     private void updateConnectionIndicator(final int target, final boolean state) {
 
         final Drawable bg = state ? getResources().getDrawable(R.drawable.rect_green)
@@ -173,12 +206,42 @@ public class MainActivity extends ActionBarActivity
             @Override
             public void run() {
                 TextView view = (TextView) findViewById(R.id.log_view);
-                CharSequence currentLogs = view.getText();
-                view.setText(currentLogs + "\n" + text);
+                view.append(text + "\n");
+
+                final int scrollAmount = view.getLayout().getLineTop(view.getLineCount()) - view.getHeight();
+                // if there is no need to scroll, scrollAmount will be <=0
+                view.scrollTo(0, scrollAmount > 0 ? scrollAmount : 0);
                 view.invalidate();
             }
         });
     }
+
+    public void updateNetStatus(final Context context) {
+        ConnectivityManager mgr = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] infos = mgr.getAllNetworkInfo();
+        boolean connected = false;
+        for (NetworkInfo info : infos) {
+            if (info.isConnected()) {
+                Log.i(TAG, "Network [" + info.getTypeName() + "] reported as connected");
+                connected = true;
+                break;
+            }
+        }
+
+        updateConnectionIndicator(R.id.net_status, connected);
+    }
+
+    public class NetworkInfoReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNetStatus(context);
+        }
+    }
+
+    /*
+     * UI Boilerplate
+     */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -202,14 +265,13 @@ public class MainActivity extends ActionBarActivity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class MainFragment extends Fragment {
+    public class MainFragment extends Fragment {
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_main, container, false);
         }
+
     }
 
 }
